114695fedc5ab0e158fb895ecce1e781347c5195
van <levan1461995@gmail.com>
2023-06-27 22:31:54 +0700

    Update code

--- week_3/3.5/orchestrate_s3.py
+++ week_3/3.5/orchestrate_s3.py
@@ -141,8 +141,8 @@ def main_flow_s3(
     mlflow.set_experiment("nyc-taxi-experiment")
 
     # Load
-    # s3_bucket_block = S3Bucket.load("s3-bucket-block")
-    # s3_bucket_block.download_folder_to_path(from_folder="data", to_folder="data")
+    s3_bucket_block = S3Bucket.load("s3-bucket-example")
+    s3_bucket_block.download_folder_to_path(from_folder="dataset", to_folder="dataset")
 
     df_train = read_data(train_path)
     df_val = read_data(val_path)
